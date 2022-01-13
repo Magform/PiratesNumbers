7b54586a47312003d7d60c12fd42836186b6a5a3
Magform <nicolas25.02.2003.nf@gmail.com>
2022-01-13 10:27:28 +0100

    Added gold earned
    Second version, is also basic but it gives the gold earned

--- autogoldcounter.py
+++ autogoldcounter.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+import tkinter
 import time
 import keyboard
 import os
@@ -5,9 +8,16 @@ import pytesseract
 import datetime
 from PIL import ImageGrab
 from PIL import Image
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" #Path to the tesseract
-OldGold=0
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+GoldRecord=open('GoldRecord.txt','r')
+last_line = GoldRecord.readlines()
+OldGold=last_line[len(last_line)-1]
+GoldRecord.close()
+OldGold= OldGold[31:]
 OldGold=int(OldGold)
+print(1)
+
 while True: 
     try:
         if keyboard.is_pressed('tab'):
@@ -20,15 +30,20 @@ while True:
             NewGold=NewGold.replace('.','')
             NewGold=NewGold.replace('','')
             NewGold=NewGold.replace(' ','')
-            NewGold=int(NewGold)
-            if OldGold!=NewGold:
-                if NewGold!=0:
-                    print(NewGold)
+            if NewGold!='':
+                NewGold=int(NewGold)
+                if OldGold!=NewGold:
                     EarnGold=NewGold-OldGold
                     EarnGold=str(EarnGold)
                     OldGold=NewGold
                     NewGold=str(NewGold)
                     currentDT = datetime.datetime.now()
+                    print(str(currentDT))
+                    print(' --> ')
+                    print(NewGold)
+                    print('(+')
+                    print(EarnGold)
+                    print(')')
                     GoldRecord=open('GoldRecord.txt','a')
                     GoldRecord.write(str(currentDT))
                     GoldRecord.write(' --> ')
@@ -37,7 +52,7 @@ while True:
                     GoldRecord.write(EarnGold)
                     GoldRecord.write(')')
                     GoldRecord.close()
-                    time.sleep(6)
-                 
+                    time.sleep(600)
+                    
     except:
         break
